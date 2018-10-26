d609c8d110cdff4aa0fbf30ae2e24e9cf29af7f5
Harley Rayner <harleymteden@gmail.com>
2018-10-26 17:48:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
    FROM series
    INNER JOIN books
    ON Series.id = Books.series_id
-   GROUP BY Series.id"
+   ORDER BY Books.year"
 end
 
 #above method is sketchy because 3 books is hard-coded
