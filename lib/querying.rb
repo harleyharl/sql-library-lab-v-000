7de2b60471fa8a516858b03ef698f154100c3866
Harley Rayner <harleymteden@gmail.com>
2018-10-26 15:57:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT Characters.species, COUNT(species)
+   FROM Characters
+   ORDER BY COUNT(species)
+   LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
