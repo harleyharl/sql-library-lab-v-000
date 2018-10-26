091d00461df19917048187f02bc4e825e666cf1a
Harley Rayner <harleymteden@gmail.com>
2018-10-26 18:11:39 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -61,6 +61,5 @@ def select_character_names_and_number_of_books_they_are_in
    JOIN Character_books
    ON Characters.id = Character_books.character_id
    GROUP BY Characters.name
-   ORDER BY COUNT(Character_books.book_id)
-   DESC LIMIT 8;"
+   ORDER BY COUNT(Character_books.book_id) DESC"
 end
