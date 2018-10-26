586fa43e66be252c005412b04a44c854e172f4ef
Harley Rayner <harleymteden@gmail.com>
2018-10-26 16:18:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT Authors.name, Subgenres.name
    FROM Authors, Series
-   LEFT JOIN Subgenres
+   INNER JOIN Subgenres
    ON Subgenres.id = Series.subgenre_id
    GROUP BY Authors.name"
 end
